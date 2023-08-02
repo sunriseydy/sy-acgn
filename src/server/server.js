5403e40b379483b0525d7426e480d0609369f311
SunriseYDY <me@sunriseydy.dev>
2023-08-02 16:04:13 +0800

    [ADD]添加 express

---
+++ src/server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import ViteExpress from "vite-express";
+
+const app = express();
+
+app.get("/", (_, res) => res.send("Hello from express!"));
+
+ViteExpress.listen(app, 9390, () =>
+    console.log(`
+🚀 Server ready at: http://localhost:9390
+⭐️ See sample requests: http://pris.ly/e/js/rest-express#3-using-the-rest-api`),
+);
