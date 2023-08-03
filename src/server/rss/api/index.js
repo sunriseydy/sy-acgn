05dea787f94bd3bdf636c68a7e0f28158a1f649a
SunriseYDY <me@sunriseydy.dev>
2023-08-03 14:48:53 +0800

    [ADD]添加rss torrent 下载

--- src/server/rss/api/index.js
+++ src/server/rss/api/index.js
@@ -5,14 +5,18 @@ import {responseWithError} from "../../utils.js";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    rssHttp.get('http://127.0.0.1/rss.xml')
+    rssHttp.rssHttp.get('http://127.0.0.1/rss.xml')
         .then(response => {
             // console.log(r.data);
-            res.send(response.data);
+            res.json(response.data);
         })
         .catch(error => {
             responseWithError(res, error);
         })
 });
+
+router.get('/download', (req, res) => {
+    rssHttp.downloadTorrentInFile('http://127.0.0.1/tmp.torrent', '/tmp/tmp.torrent', res)
+})
 
 export default router;
