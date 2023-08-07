12e60e1c0e99d7dfe8253f4764f10b238750b519
SunriseYDY <me@sunriseydy.dev>
2023-08-07 20:54:19 +0800

    [ADD]工具类添加成功和失败的响应处理

--- src/server/utils.js
+++ src/server/utils.js
@@ -1,5 +1,18 @@
 const responseWithError = (res, error) => {
-    res.status(500).send(error);
+    res.status(500)
+        .json({
+            success: false,
+            message: error.message || error,
+            data: error
+        })
 }
 
-export {responseWithError};
+const responseWithSuccess = (res, message, data) => {
+    res.json({
+        success: true,
+        message: message || '成功',
+        data: data
+    })
+}
+
+export {responseWithError, responseWithSuccess};
