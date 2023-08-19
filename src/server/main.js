1043b94e5351e46d86178554bc08e99a5f1676f7
SunriseYDY <me@sunriseydy.dev>
2023-08-19 21:40:47 +0800

    [ADD]添加cors配置

--- src/server/main.js
+++ src/server/main.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import ViteExpress from 'vite-express'
 import rssApi from './rss/rssApi.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/rss', rssApi)
 app.get('/', (_, res) => res.json('Hello from express!'))
