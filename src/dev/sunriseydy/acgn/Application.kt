b6b4cde6b70f7f5ffa0eff1ab29c6497544bcb6b
SunriseYDY <me@sunriseydy.dev>
2024-06-15 01:00:59 +0800

    [add][s]添加忽略编译警告

--- src/dev/sunriseydy/acgn/Application.kt
+++ src/dev/sunriseydy/acgn/Application.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
+@Suppress("unused")
 fun Application.module() {
     configureSerialization()
     configureDatabases()
