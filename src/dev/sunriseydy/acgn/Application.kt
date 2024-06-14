0e039de3c5bed9e3df7fe16e5b2a64b0b3578c05
SunriseYDY <me@sunriseydy.dev>
2024-06-15 00:34:50 +0800

    [ref][s]修改 server engine

--- src/dev/sunriseydy/acgn/Application.kt
+++ src/dev/sunriseydy/acgn/Application.kt
@@ -3,9 +3,7 @@ package dev.sunriseydy.acgn
 import dev.sunriseydy.acgn.plugins.*
 import io.ktor.server.application.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
 fun Application.module() {
     configureSerialization()
