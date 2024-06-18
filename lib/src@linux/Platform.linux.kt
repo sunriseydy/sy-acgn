063e311d9c841de2123bca758d2388fe429aa804
SunriseYDY <me@sunriseydy.dev>
2024-06-18 23:14:20 +0800

    [ref]删掉linux模块，添加wasm模块

--- lib/src@linux/Platform.linux.kt
+++
@@ -1,7 +0,0 @@
-package dev.sunriseydy.acgn
-
-class LinuxPlatform: Platform {
-    override val name: String = "Linux"
-}
-
-actual fun getPlatform(): Platform = LinuxPlatform()
---
+++ lib/src@wasm/Platform.wasmJs.kt
@@ -0,0 +1,7 @@
+package dev.sunriseydy.acgn
+
+class WasmPlatform: Platform {
+    override val name: String = "Web with Kotlin/Wasm"
+}
+
+actual fun getPlatform(): Platform = WasmPlatform()
