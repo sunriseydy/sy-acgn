14457ea0f7776443e2419b666501a29a174a0e18
SunriseYDY <me@sunriseydy.dev>
2024-06-22 22:30:13 +0800

    [ref]删掉vue-app

--- vue-app/src/plugins/router.ts
+++
@@ -1,8 +0,0 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import anime from '@/routes/anime'
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [...anime],
-})
-export default router
--- vue-app/src/routes/anime.ts
+++
@@ -1,1 +0,0 @@
-export default [{ path: '/anime/rss', component: () => import('@/pages/anime/AnimeRss.vue') }]
