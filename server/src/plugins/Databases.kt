e6189382368e75da306fe4ed997e9a2b8d8054ed
SunriseYDY <me@sunriseydy.dev>
2024-06-28 09:35:05 +0800

    [imp][server]去掉不使用的代码

--- server/src/plugins/Databases.kt
+++ server/src/plugins/Databases.kt
@@ -11,7 +11,6 @@ fun Application.configureDatabases() {
 }
 
 fun Application.connectToPostgres() {
-    Class.forName("org.postgresql.Driver")
     val user = environment.config.property("acgn.postgres.user").getString()
     val password = environment.config.property("acgn.postgres.password").getString()
     val host = environment.config.property("acgn.postgres.host").getString()
