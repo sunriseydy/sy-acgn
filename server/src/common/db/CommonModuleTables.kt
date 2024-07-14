851714af8521201c3706e784b3e312dbff1abc5b
SunriseYDY <me@sunriseydy.dev>
2024-07-14 19:30:29 +0800

    [feat]添加 config table

--- server/src/common/db/CommonModuleTables.kt
+++ server/src/common/db/CommonModuleTables.kt
@@ -1,9 +1,13 @@
 package dev.sunriseydy.acgn.common.db
 
 import dev.sunriseydy.acgn.common.dto.AdditionalInfo
+import dev.sunriseydy.acgn.common.dto.AppConfig
+import org.jetbrains.exposed.dao.ULongEntity
+import org.jetbrains.exposed.dao.ULongEntityClass
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.ULongIdTable
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.kotlin.datetime.CurrentTimestamp
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
@@ -43,4 +47,28 @@ class AdditionalInfoDAO(id: EntityID<UUID>) : UUIDEntity(id) {
     )
 }
 
-fun commonModuleTables() = listOf(AdditionalInfoTable)
+object AppConfigTable : ULongIdTable("common_config") {
+    val configKey = varchar("config_key", 256)
+    val configValue = text("config_value", eagerLoading = true)
+    val createdAt = timestamp("created_at").defaultExpression(CurrentTimestamp)
+    val updatedAt = timestamp("updated_at").defaultExpression(CurrentTimestamp)
+}
+
+class AppConfigDAO(id: EntityID<ULong>) : ULongEntity(id) {
+    companion object : ULongEntityClass<AppConfigDAO>(AppConfigTable)
+
+    var configKey by AppConfigTable.configKey
+    var configValue by AppConfigTable.configValue
+    var createdAt by AppConfigTable.createdAt
+    var updatedAt by AppConfigTable.updatedAt
+
+    fun toDTO() = AppConfig(
+        id = id.value,
+        configKey = configKey,
+        configValue = configValue,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
+}
+
+fun commonModuleTables() = listOf(AdditionalInfoTable, AppConfigTable)
