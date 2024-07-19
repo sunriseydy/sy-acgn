07fa56e46bc2ec45579a050c0ccdf9c71caa0dc1
SunriseYDY <me@sunriseydy.dev>
2024-07-19 18:02:28 +0800

    [feat]AppConfig添加Serializable

--- lib/src/common/dto/AppConfig.kt
+++ lib/src/common/dto/AppConfig.kt
@@ -1,11 +1,13 @@
 package dev.sunriseydy.acgn.common.dto
 
 import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 
 /**
  * @author SunriseYDY
  * @date 2024-07-12 13:14
  */
+@Serializable
 data class AppConfig(
     val id: ULong,
     val configKey: String,
