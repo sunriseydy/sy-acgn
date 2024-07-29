0dfc1b51ad6a2c5e9aae8f0b4b96740cdee1c34e
SunriseYDY <me@sunriseydy.dev>
2024-07-30 00:10:20 +0800

    [feat][lib] add persist setting

--- lib/src/tools/AppConfigTool.kt
+++ lib/src/tools/AppConfigTool.kt
@@ -1,5 +1,7 @@
 package dev.sunriseydy.acgn.tools
 
+import com.russhwolf.settings.Settings
+import dev.sunriseydy.acgn.common.config.CommonModuleAppConfig
 import dev.sunriseydy.acgn.common.dto.AppConfig
 
 /**
@@ -7,7 +9,16 @@ import dev.sunriseydy.acgn.common.dto.AppConfig
  * @date 2024-07-12 16:47
  */
 object AppConfigTool {
-    private val appConfigs = mutableMapOf<String/* config key */, Pair<AppConfig?/* db value */, String?/* file value */>>()
+    /**
+     * 持久化的配置
+     */
+    private val serverSettings: Settings = Settings()
+
+    fun getLocalServerConfig(): String? = serverSettings.getStringOrNull(CommonModuleAppConfig.AppServer.configKey)
+    fun setLocalServerConfig(value: String) = serverSettings.putString(CommonModuleAppConfig.AppServer.configKey, value)
+
+    private val appConfigs =
+        mutableMapOf<String/* config key */, Pair<AppConfig?/* db value */, String?/* file value */>>()
 
     fun getAppConfigStringValue(key: String): String? {
         return appConfigs[key]?.first?.configValue ?: appConfigs[key]?.second
--- ui/src/App.kt
+++ ui/src/App.kt
@@ -3,12 +3,18 @@ package dev.sunriseydy.acgn.ui
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import dev.sunriseydy.acgn.tools.AppConfigTool
 import dev.sunriseydy.acgn.ui.navigation.AcgnNavigationWrapper
 
 @Composable
 fun App() {
     MaterialTheme {
         Surface {
+            val server = AppConfigTool.getLocalServerConfig()
+            println("server: $server")
+            if (server == null) {
+                AppConfigTool.setLocalServerConfig("test")
+            }
             AcgnNavigationWrapper()
         }
     }
