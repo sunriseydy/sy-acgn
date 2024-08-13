22f3063b831947f7838cd49976ba2ba107c49a7a
SunriseYDY <me@sunriseydy.dev>
2024-08-13 13:10:29 +0800

    [feat][client] 应用全屏

--- desktop-client/src/DesktopClientMain.kt
+++ desktop-client/src/DesktopClientMain.kt
@@ -1,12 +1,17 @@
 package dev.sunriseydy.acgn.client
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val windowState: WindowState = rememberWindowState(placement = WindowPlacement.Maximized)
     Window(
         onCloseRequest = ::exitApplication,
-        title = "SY ACGN"
+        state = windowState,
+        title = "SY ACGN",
     ) {
         App()
     }
