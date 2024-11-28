20fe8e1575801049cb475f825d98805a1a8b3fee
SunriseYDY <me@sunriseydy.dev>
2024-11-29 00:51:30 +0800

    ⬆️ 升级 navigation 和 viewmodel

--- desktop-client/src/DesktopClientMain.kt
+++ desktop-client/src/DesktopClientMain.kt
@@ -2,22 +2,17 @@ package dev.sunriseydy.acgn.client
 
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.window.singleWindowApplication
 
-fun main() = application {
-    val windowState: WindowState = rememberWindowState(
-        placement = WindowPlacement.Maximized,
-        size = DpSize(width = 1200.dp, height = 1000.dp),
-    )
-    Window(
-        onCloseRequest = ::exitApplication,
-        state = windowState,
+fun main() =
+    singleWindowApplication(
         title = "SY ACGN",
+        state = WindowState(
+            placement = WindowPlacement.Maximized,
+            size = DpSize(width = 1200.dp, height = 1000.dp),
+        ),
     ) {
         App()
     }
-}
