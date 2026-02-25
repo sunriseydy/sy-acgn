0c8c3a1e89eb189f5691ed3d52f95709b7b41c61
SunriseYDY <me@sunriseydy.dev>
2026-02-25 15:32:45 +0800

    ✨ 添加 Android 日志功能，集成 logback-android 并配置日志输出

--- android-client/src/MainActivity.kt
+++ android-client/src/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dev.sunriseydy.acgn.client.App
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val logger = KotlinLogging.logger { }
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        logger.info { "MainActivity onCreate" }
         super.onCreate(savedInstanceState)
         setContent {
             App()
